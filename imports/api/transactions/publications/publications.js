144364cb48956522f71cb81fb3f4b9b29c7e54e8
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-11 18:05:13 -0400

    Deleting unnecessary methods

--- imports/api/transactions/publications/publications.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-import { Transactions } from '../transactions.js';
-
-Meteor.publish('todos', function todosPublication() {
-  return Transactions.find();
-});
