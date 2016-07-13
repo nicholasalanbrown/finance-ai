c196eb7154b403f65fb602004fa9d65b5f20c2ca
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-12 23:41:12 -0400

    Handling singular and plural

--- imports/api/transactions/methods.js
+++ imports/api/transactions/methods.js
@@ -109,7 +109,8 @@ Meteor.methods({
     else {
       const amounts = _.pluck(docArray, 'amount');
       const sum = _.reduce(amounts, function(memo, num){ return memo + num; }, 0);
-      speech = 'I found ' + docArray.length + ' transactions, totalling $' + sum;
+      let transactionNoun = docArray.length > 1 ? 'transactions' : 'transaction';
+      speech = 'I found ' + docArray.length +' '+ transactionNoun +', totalling $' + sum;
     }
 
     console.log(speech);
@@ -124,7 +125,6 @@ Meteor.methods({
 
   webhook(response) {
     console.log('Running webhook method...');
-    History.insert(response);
     let calledFunction;
     switch (response.result.action) {
       case 'getTransactions':
@@ -151,75 +151,4 @@ Meteor.methods({
     }
     return calledFunction;
   },
-
 })
-
-/*
-
-Meteor.methods({
-  getBalance() {
-    return {
-      speech: 'Your checking account balance is $500',
-      displayText: 'Your checking account balance is $500',
-      data: {},
-      contextOut: [],
-    };
-  },
-  getSpending(category, start, end) {
-    console.log(category, start, end);
-    return {
-      speech: 'getSpending',
-      displayText: 'getSpending',
-      data: {},
-      contextOut: [],
-    };
-  },
-  getTransactions(date) {
-    let convertedDate = moment(date).format();
-    console.log(convertedDate);
-    return {
-      speech: 'getTransactions',
-      displayText: 'getTransactions',
-      data: {},
-      contextOut: [],
-    };
-  },
-  webhook(response) {
-    console.log(response);
-    History.insert(response);
-    let calledFunction;
-    switch (response.result.action) {
-      case 'getTransactions':
-        const dateExists = (response, result, paramaters, date, calendar) => {
-          const args = Array.prototype.slice.call(arguments, 1);
-          for (var i = 0; i < args.length; i++) {
-            if (!obj || !obj.hasOwnProperty(args[i])) {
-              return false;
-            }
-            obj = obj[args[i]];
-          }
-          return true;
-        }
-        if (dateExists) {
-          calledFunction = Meteor.call('getTransactions', result.paramaters.date.calendar);
-        }
-        break;
-      case 'getSpendingf':
-        calledFunction = Meteor.call('getSpending')
-        break;
-      case 'getBalance':
-        calledFunction = Meteor.call('getBalance');
-        break;
-      default:
-        calledFunction = {
-          speech: "Sorry, I couldn't find a function associated with that intent",
-          displayText: "Sorry, I couldn't find a function associated with that intent",
-          data: {},
-          contextOut: [],
-        };
-    }
-    return calledFunction;
-  },
-});
-
-*/
