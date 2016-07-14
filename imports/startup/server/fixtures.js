cfdef38de8048f14c4c0d47f8291cf457cddece8
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-14 16:58:06 -0400

    CLoser to plaid integration

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Transactions } from '../../api/transactions/transactions.js';
 let mintData = require('./mint-data.json');
+import plaid from 'plaid';
+const plaidClient = new plaid.Client(Meteor.settings.plaid_client_id, Meteor.settings.plaid_secret, plaid.environments.tartan);
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
