fc36240e464ee11e45913f1198f06cc913a8260e
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-15 11:13:36 -0400

    Adding Plaid LInk

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,6 +6,16 @@ const plaidClient = new plaid.Client(Meteor.settings.plaid_client_id, Meteor.set
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
+  plaidClient.addAuthUser('schwab', {username: 'nicholasalanbrown', password: 'ach1tung'}, function (error, response) {
+    if (error) {
+      console.log ("Error!");
+      console.log(error);
+    }
+    else {
+      console.log("Success!");
+      console.log(response);
+    }
+  });
   if (Transactions.find().count() === 0) {
     mintData.forEach((transaction) => {
       const transactionId = Transactions.insert(transaction);
