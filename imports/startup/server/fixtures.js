d7765f67c00e3228dc1b0e1c79a486609fcb5ba3
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-17 16:52:58 -0400

    Removing some Plaid tests

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,21 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Transactions } from '../../api/transactions/transactions.js';
 let mintData = require('./mint-data.json');
-import plaid from 'plaid';
-const plaidClient = new plaid.Client(Meteor.settings.plaid_client_id, Meteor.settings.plaid_secret, plaid.environments.tartan);
 
-// if the database is empty on server start, create some sample data.
+
 Meteor.startup(() => {
-  plaidClient.addAuthUser('schwab', {username: 'nicholasalanbrown', password: 'ach1tung'}, function (error, response) {
-    if (error) {
-      console.log ("Error!");
-      console.log(error);
-    }
-    else {
-      console.log("Success!");
-      console.log(response);
-    }
-  });
   if (Transactions.find().count() === 0) {
     mintData.forEach((transaction) => {
       const transactionId = Transactions.insert(transaction);
