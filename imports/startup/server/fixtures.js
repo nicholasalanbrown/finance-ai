626fbfadcb78f2c05d891e61309cb2481cb7a103
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-17 23:09:24 -0400

    Adding utility parsing function

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -3,20 +3,66 @@ import { Transactions } from '../../api/transactions/transactions.js';
 import jsonfile from 'jsonfile';
 import _ from 'underscore';
 import moment from 'moment';
-const transactions = require('./transactions.json');
+const transactionTemplate = require('./transaction-template.json');
+const mintData = require('./mint-data.json');
+const categoryMappings = require('./category-mappings.json');
 const base = process.env.PWD;
 const path = require('path');
 const fs = require('fs');
 
-Meteor.startup(() => {
+Meteor.methods({
+  transformMintData(data, filename) {
 
-  if (Transactions.find().count() === 0) {
+    let jsonArray = [];
 
-    _.each(transactions, function(transaction) {
-      Transactions.insert(transaction);
+    let newTransaction = transactionTemplate;
+    _.each(data, function(mint) {
+
+      newTransaction.name = mint.description;
+      newTransaction.date = moment(mint.date).toISOString();
+      newTransaction.amount = mint.amount;
+
+      let mappedCategory = _.findWhere(categoryMappings, {mint: mint.category});
+      let categoryArray = [];
+
+      if (mappedCategory) {
+
+        if (mappedCategory.plaid1) {
+          categoryArray.push(mappedCategory.plaid1);
+        }
+        if (mappedCategory.plaid2) {
+          categoryArray.push(mappedCategory.plaid2);
+        }
+        if (mappedCategory.plaid3) {
+          categoryArray.push(mappedCategory.plaid3);
+        }
+
+      }
+
+      if (!mappedCategory && mint.category != 'Uncategorized') {
+        console.log('Could not find category mapping for '+mint.category);
+      }
+
+      newTransaction.category = categoryArray;
+
+      jsonArray.push(newTransaction);
+
+    });
+
+    jsonfile.writeFile(base + __dirname + filename, jsonArray, function (err) {
+      if (err) {
+        console.error(err)
+      }
+      else {
+        console.log('File written successfully to '+ __dirname + filename);
+      }
+
     })
 
   }
+});
 
+
+Meteor.startup(() => {
 
 });
