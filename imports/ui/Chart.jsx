1e2421f0bda52936d7a49e0a17409c35804e0099
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-07-25 14:34:14 -0400

    Better chart labels

--- imports/ui/Chart.jsx
+++ imports/ui/Chart.jsx
@@ -2,17 +2,24 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import ChartistGraph from 'react-chartist';
 import { _ } from 'meteor/underscore';
+import moment from 'moment';
 
 export default class Chart extends Component {
 
   render() {
 
     const amounts = _.pluck(this.props.data, 'amount');
+    const dates = _.pluck(this.props.data, 'date');
+    const labels = _.map(dates, function (date) {
+      return moment(date).format('MMMM Do');
+    });
     const series = _.map(amounts, function(amount) {
       return amount*-1;
     });
+
+    console.log(labels);
     const data = {
-      labels: series,
+      labels: labels,
       series: [series]
     };
 
